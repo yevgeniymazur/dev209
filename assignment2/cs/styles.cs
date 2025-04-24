eb3382e84db117e6673cd4de243be916f28d6e49
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-04-23 19:07:59 -0700

    Update styles.cs

--- assignment2/cs/styles.cs
+++ assignment2/cs/styles.cs
@@ -1,1 +1,43 @@
 
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: hsl(212, 45%, 89%);
+  font-family: 'Outfit', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.card {
+  background-color: hsl(0, 0%, 100%);
+  padding: 1.5rem;
+  border-radius: 1rem;
+  max-width: 320px;
+  text-align: center;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.05);
+}
+
+.qr-image {
+  width: 100%;
+  border-radius: 0.75rem;
+  margin-bottom: 1rem;
+}
+
+h1 {
+  font-size: 1.25rem;
+  color: hsl(218, 44%, 22%);
+  font-weight: 700;
+  margin-bottom: 0.75rem;
+}
+
+p {
+  font-size: 15px;
+  color: hsl(216, 15%, 48%);
+  font-weight: 400;
+}
