76a9b2967e6567441993ac7e2f6a42de6fef3743
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-05-21 21:18:07 -0700

    Update header.js

--- assignment5/js/header.js
+++ assignment5/js/header.js
@@ -1,4 +1,16 @@
-// js/header.js
 export function Header() {
-  return React.createElement('h1', null, 'My Todo List');
+  return React.createElement('div', null, [
+    React.createElement('img', {
+      key: 'logo',
+      src: 'images/kinglogo.png',
+      alt: 'King Logo',
+      style: {
+        width: '120px',
+        display: 'block',
+        margin: '0 auto 20px auto',
+        borderRadius: '8px'
+      }
+    }),
+    React.createElement('h1', { key: 'title' }, 'My Todo List')
+  ]);
 }
