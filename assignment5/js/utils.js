a943bc465cd7cbc97eea26dd7bdf2ba6cb4a0128
yevgeniymazur <119826811+yevgeniymazur@users.noreply.github.com>
2025-05-21 20:57:58 -0700

    Create utils.js

---
+++ assignment5/js/utils.js
@@ -0,0 +1,17 @@
+// js/utils.js
+
+export function setCookie(name, value, days = 1) {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
+}
+
+export function getCookie(name) {
+  return document.cookie
+    .split('; ')
+    .find(row => row.startsWith(name + '='))
+    ?.split('=')[1];
+}
+
+export function deleteCookie(name) {
+  document.cookie = `${name}=; Max-Age=0; path=/`;
+}
